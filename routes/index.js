e1c1c0fae9b919ba3197f5589197364e78a62713
vutanguofa <68674610+vutanguofa@users.noreply.github.com>
2020-12-06 11:51:41 -0700

    including routes details

--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,13 @@
 // });
 
 // module.exports = router;
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+    res.status(404).json({ message: 'Page not found!' })
+});
+
+module.exports = router;
